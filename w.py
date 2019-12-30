4bacbf6adbb9d4c605e7eb9a5260e05c3f4af59f
SkullCandby <42152517+SkullCandby@users.noreply.github.com>
2019-12-30 11:52:26 +0300

    Писал в другом проекте, потом перенёс сюда

--- w.py
+++ w.py
@@ -28,16 +28,44 @@ def load_level(filename):
     print(list(map(lambda x: x.ljust(max_width, '.'), level_map)))
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
-
+flag_moove = True
 def terminate():
     pygame.quit()
     sys.exit()
 
 
+class AnimatedSprite(pygame.sprite.Sprite):
+    player_move_flag = 1
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect.x = 422
+        self.rect.y = 501
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        '''while AnimatedSprite.player_move_flag:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+
+            self.image = self.frames[self.cur_frame]'''
+
+
+
 flag = True
 
 
-def start_screen():
+'''def start_screen():
     global flag
     while flag:
         fon = pygame.transform.scale(load_image('fon.jpg'), (w, h))
@@ -47,15 +75,20 @@ def start_screen():
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 flag = flag and False
-        pygame.display.flip()
+        pygame.display.flip()'''
 
+tile_images = {'r_wall': load_image('right_wall.jpg'),
+               'l_wall': load_image('left_wall.jpg'),
+               'full_window': load_image('full_okno.jpg'),
+               'damaged_window': load_image('damaged_okno.jpg'),
+               'nefull_window': load_image('nefull_okno.jpg')}
+player_image1 = load_image('ralf_2.gif')
+player_image = pygame.transform.scale(player_image1, (187, 87))
 
-tile_images = {'wall': load_image('dom1.jpg')}
-player_image1 = load_image('ralf_1.png')
-player_image = pygame.transform.scale(player_image1, (-1000, +50))
-
-tile_width = 424
+tile_width = 71
 tile_height = 114
+wall_width = 39
+wall_height = 112
 player = None
 
 all_sprites = pygame.sprite.Group()
@@ -71,23 +104,36 @@ class Tile(pygame.sprite.Sprite):
 
 
 class Player(pygame.sprite.Sprite):
+    player_moove_flag = True
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-
+        self.rect = self.image.get_rect()
+        # self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        self.rect.x = 422
+        self.rect.y = 501
 def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '#':
-                Tile('wall', x, y)
+                Tile('nefull_window', x, y)
             elif level[y][x] == '@':
-                Tile('wall', x, y)
-                new_player = Player(x, y)
+                player = AnimatedSprite(load_image("felix_move_spritelist.png"), 3, 1, 697, 475)
+                Tile('full_window', x, y)
+                '''new_player = Player(x, y)'''
+            elif level[y][x] == '|':
+                Tile('wall2', x, y)
+            elif level[y][x] == '%':
+                Tile('damaged_window', x, y)
+            elif level[y][x] == '.':
+                Tile('full_window', x, y)
+            elif level[y][x] == ')':
+                Tile('r_wall', x, y)
+            elif level[y][x] == '(':
+                Tile('l_wall', x, y)
 
-    return new_player, x, y
+    return x, y
 
 
 class Camera:
@@ -104,9 +150,18 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h // 2 - h // 2)
 
 
-player, level_x, level_y = generate_level(load_level('ralf_map.txt'))
+fon = load_image('earth.jpg')
+
+level_x, level_y = generate_level(load_level('ralf_map.txt'))
+player = AnimatedSprite(load_image("heroes.png"), 4, 2, 697, 475)
+print(type(player))
+
+'''player.rect.x = 486
+player.rect. = 561'''
 camera = Camera()
 running = True
+hh = 640
+clock = pygame.time.Clock()
 while running:
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -114,21 +169,29 @@ while running:
             pygame.quit()
             sys.exit()
         if keys[pygame.K_LEFT]:
-            player.rect.x -= 10
+            player.rect.x -= 70
         elif keys[pygame.K_RIGHT]:
-            player.rect.x += 10
+            AnimatedSprite.player_move_flag = True
+            player.update()
+            player.rect.x += 70
+            AnimatedSprite.player_move_flag = False
         elif keys[pygame.K_UP]:
-            player.rect.y -= 10
+            hh += 114
+            player.rect.y -= 114
         elif keys[pygame.K_DOWN]:
-
-            player.rect.y += 10
-            print(player.rect.x, player.rect.y)
-
+            hh -= 114
+            player.rect.y += 114
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
     screen.fill((0, 0, 0))
-    start_screen()
+    # start_screen()
     camera.update(player)
     all_sprites.draw(screen)
     for sprite in all_sprites:
         camera.apply(sprite)
+    player_group.draw(screen)
+
+    screen.blit(fon, (0, hh))
+    pygame.time.delay(150)
     pygame.display.flip()
 pygame.quit()
