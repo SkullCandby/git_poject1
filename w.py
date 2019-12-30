dff46e2f964f484245706fe3094a07977bcae2bb
SkullCandby <42152517+SkullCandby@users.noreply.github.com>
2019-12-30 12:17:38 +0300

    убрал start_screen()

--- w.py
+++ w.py
@@ -42,8 +42,9 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.cut_sheet(sheet, columns, rows)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
-        self.rect.x = 422
-        self.rect.y = 501
+        self.rect.x = x
+        self.rect.y = y
+        self.mask = pygame.mask.from_surface(self.image)
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -63,20 +64,6 @@ class AnimatedSprite(pygame.sprite.Sprite):
 
 
 flag = True
-
-
-'''def start_screen():
-    global flag
-    while flag:
-        fon = pygame.transform.scale(load_image('fon.jpg'), (w, h))
-        screen.blit(fon, (0, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                flag = flag and False
-        pygame.display.flip()'''
-
 tile_images = {'r_wall': load_image('right_wall.jpg'),
                'l_wall': load_image('left_wall.jpg'),
                'full_window': load_image('full_okno.jpg'),
@@ -101,10 +88,13 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos_x
+        self.rect.y = pos_y
 
 
 class Player(pygame.sprite.Sprite):
-    player_moove_flag = True
+    player_move_flag = True
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
@@ -153,9 +143,7 @@ class Camera:
 fon = load_image('earth.jpg')
 
 level_x, level_y = generate_level(load_level('ralf_map.txt'))
-player = AnimatedSprite(load_image("heroes.png"), 4, 2, 697, 475)
-print(type(player))
-
+player = AnimatedSprite(load_image("felix_move_spritelist.png"), 2, 1, 697, 475)
 '''player.rect.x = 486
 player.rect. = 561'''
 camera = Camera()
@@ -168,24 +156,27 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if keys[pygame.K_LEFT]:
-            player.rect.x -= 70
-        elif keys[pygame.K_RIGHT]:
-            AnimatedSprite.player_move_flag = True
-            player.update()
-            player.rect.x += 70
-            AnimatedSprite.player_move_flag = False
-        elif keys[pygame.K_UP]:
-            hh += 114
-            player.rect.y -= 114
-        elif keys[pygame.K_DOWN]:
-            hh -= 114
-            player.rect.y += 114
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
+        if pygame.sprite.collide_mask(player_group, tiles_group):
+            if keys[pygame.K_LEFT]:
+                player.rect.x -= 70
+            elif keys[pygame.K_RIGHT]:
+                AnimatedSprite.player_move_flag = True
+                player.update()
+                player.rect.x += 70
+                AnimatedSprite.player_move_flag = False
+            elif keys[pygame.K_UP]:
+                hh += 114
+                player.rect.y -= 114
+            elif keys[pygame.K_DOWN]:
+                hh -= 114
+                player.rect.y += 114
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
     screen.fill((0, 0, 0))
     # start_screen()
     camera.update(player)
+
+    tiles_group.draw(screen)
     all_sprites.draw(screen)
     for sprite in all_sprites:
         camera.apply(sprite)
